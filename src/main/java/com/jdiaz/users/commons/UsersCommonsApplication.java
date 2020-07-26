5daecda55a36d883d67b94e957557e9b763258b4
Jesus Diaz <jesus740@gmail.com>
2020-07-26 11:35:12 -0500

    initial commit

---
+++ src/main/java/com/jdiaz/users/commons/UsersCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.jdiaz.users.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class UsersCommonsApplication {
+
+
+
+}
